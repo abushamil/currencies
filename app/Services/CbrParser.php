450de35ba8c5b5ffec75d8b5e1f5a27954a24dc0
Almaz <almazrt@gmail.com>
2021-03-03 17:24:15 +0300

    fix cbr parser

--- app/Services/CbrParser.php
+++ app/Services/CbrParser.php
@@ -5,6 +5,8 @@ namespace App\Services;
 
 
 use App\Contracts\CbrCurrency;
+use Illuminate\Support\Facades\Http;
+use SimpleXMLElement;
 
 class CbrParser
 {
@@ -18,12 +20,35 @@ class CbrParser
     /**
      * @return CbrCurrency[]
      */
-    public function getCurrencies(): array{
-        return [];
+    public function getCurrencies(): array
+    {
+        $currencies = [];
+        $xml = $this->getXmlCurrencies();
+        foreach ($xml->Valute as $row) {
+            $currencies[] = $this->serializeCurrency($row);
+        }
+        return $currencies;
     }
 
-    public function parse():string{
+    public function getRaw(): string
+    {
+        return Http::get($this->feedUrl);
+    }
 
+    public function getXmlCurrencies(): SimpleXMLElement
+    {
+        return new SimpleXMLElement($this->getRaw());
+    }
+
+    public function serializeCurrency(SimpleXMLElement $row): CbrCurrency
+    {
+        $currency = new CbrCurrency();
+        $currency->numCode = (int)$row->NumCode;
+        $currency->charCode = $row->CharCode;
+        $currency->nominal = (int)$row->Nominal;
+        $currency->name = $row->Name;
+        $currency->value = (float)$row->Value;
+        return $currency;
     }
 
 }
